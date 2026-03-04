680f3b3ed60b1f1e583938bed6a9b0848855f999
Isami_noir <ibnusalam290902@gmail.com>
2026-03-04 08:49:39 +0700

    Fix unused App import

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import App from './App.tsx'
 import Home from './pages/Home.tsx';
 
 createRoot(document.getElementById('root')!).render(
